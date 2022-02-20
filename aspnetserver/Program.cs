aec6e95f0859d1e3a5c021d721d63ee070f61552
mika1407 <koneautomaatio@hotmail.com>
2022-02-20 11:52:49 +0200

    cors lisätty backendiin

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -3,6 +3,18 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy",
+        builder =>
+    {
+        builder
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+    });
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,6 +34,8 @@ app.UseSwaggerUI( swaggerUiOptions =>
 });
 
 app.UseHttpsRedirection();
+
+app.UseCors("CORSPolicy");
 
 app.MapGet("/get-all-posts", async () => await PostRepository.GetPostsAsync())
     .WithTags("Posts Endpoints");
